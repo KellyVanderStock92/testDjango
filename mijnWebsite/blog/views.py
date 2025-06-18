44d9cab8cd9dc3b888b1f18d984695971057887e
KellyVanderStock <kelly.vanderstock@hotmail.com>
2025-06-18 19:27:33 +0200

    html toegevoegd, css toegevoegd, belangrijke aanpassing: staticfiles_dir en static_root in settings.py aangepast omdat css niet werd ingeladen

--- mijnWebsite/blog/views.py
+++ mijnWebsite/blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from django.utils import timezone # importeer render en timezone uit Django.shortcuts en Django.utils
+from .models import Post #importeer het Post model uit de models.py file
 # Create your views here.
 
 #schrijf een fuunctie die de waarden van de post_list view teruggeeft
 def post_list(request):
-    return render(request, 'blog/post_list.html', {}) # template post_list.html wordt gerenderd en wordt opgevuld met de inhoud, dit wordt dan aan bezoeker getoond als mooie pagina
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') # haal alle posts op die gepubliceerd zijn en sorteer ze op de publicatiedatum
+    return render(request, 'blog/post_list.html', {'posts':posts}) # template post_list.html wordt gerenderd en wordt opgevuld met de inhoud, dit wordt dan aan bezoeker getoond als mooie pagina
+#de query die we hebben geschreven vertegenwoordigt een verzameling van Post objecten uit de database die voldoen aan de filtercriteria: publicatiedatum
 
 
