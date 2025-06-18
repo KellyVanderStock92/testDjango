3261d08091e61f893d0fdc397015f3ec0dce6586
KellyVanderStock <kelly.vanderstock@hotmail.com>
2025-06-18 10:57:04 +0200

    Huiswerk: model VOETBALSPELERS aangemaakt en nodige aanpassingen gedaan in models.py en admin.py

--- mijnWebsite/blog/admin.py
+++ mijnWebsite/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Voetbalspelers
 #model registratie in de admin interface
-admin.site.register(Post)
+admin.site.register(Voetbalspelers)
 # Register your models here.
