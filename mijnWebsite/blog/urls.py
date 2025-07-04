1ce90004220ef73ec3375721bc11e29b97f1ef3f
KellyVanderStock <kelly.vanderstock@hotmail.com>
2025-07-04 10:14:47 +0200

    les 6: base.html, post_list.html, post_inhoud.html, urls.py, views.py aangemaakt en aangepast waar nodig

--- mijnWebsite/blog/urls.py
+++ mijnWebsite/blog/urls.py
@@ -3,4 +3,5 @@ from . import views # import views uit de huidige map
 # urlpatterns is een lijst van url patronen die de views aanroepen
 urlpatterns = [
     path('', views.post_list, name='post_list'),  # de post_list view wordt aangeroepen wanneer de root url wordt bezocht
+    path('post/<int:pk>/', views.post_detail, name='post_inhoud'),  # de post_detail view wordt aangeroepen wanneer een specifieke post wordt bezocht
 ]
--- mijnWebsite/blog/views.py
+++ mijnWebsite/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone # importeer render en timezone uit Django.shortcuts en Django.utils
+from django.shortcuts import render, get_object_or_404 
 from .models import Post #importeer het Post model uit de models.py file
 # Create your views here.
 
@@ -9,4 +10,7 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts':posts}) # template post_list.html wordt gerenderd en wordt opgevuld met de inhoud, dit wordt dan aan bezoeker getoond als mooie pagina
 #de query die we hebben geschreven vertegenwoordigt een verzameling van Post objecten uit de database die voldoen aan de filtercriteria: publicatiedatum
 
-
+#schrijf een functie die de waarden van de post_detail view teruggeeft
+def post_detail(request, pk): # pk staat voor primary key, dit is een unieke identifier voor elk Post object
+    post=get_object_or_404(Post, pk=pk) # haal het Post object op met de opgegeven primary key, als het niet bestaat, geef dan een 404 foutmelding
+    return render(request, 'blog/post_inhoud.html', {'post': post}) # template post_inhoud.html wordt gerenderd en wordt opgevuld met de inhoud van het Post object, dit wordt dan aan bezoeker getoond als mooie pagina. template aanmaken in de templates/blog folder
